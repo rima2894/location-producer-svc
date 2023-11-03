ccde65bda474d6cae2717d62a400ef3cbd6c1229
MCH495 <madhurima.chatterjee@lns.maersk.com>
2023-11-03 15:24:37 +0530

    Initial Commit

---
+++ src/main/java/com/example/locationproducersvc/LocationProducerSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationproducersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationProducerSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationProducerSvcApplication.class, args);
+	}
+
+}
