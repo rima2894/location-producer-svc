baf31642bfe6c3f83b0bf61dcf7b89597f574e0d
MCH495 <madhurima.chatterjee@lns.maersk.com>
2023-11-14 13:04:12 +0530

    Committing reactive producer code

---
+++ src/main/java/com/example/locationproducersvc/controller/Controller.java
@@ -0,0 +1,20 @@
+package com.example.locationproducersvc.controller;
+
+import com.example.locationproducersvc.service.AzureBlobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class Controller {
+	@Autowired
+	private AzureBlobService azureBlobService;
+
+	@GetMapping("/loadJson")
+	public  Mono<String> loadJson()
+	{
+		 return azureBlobService.readExcelFromAzureBlobAndArchive();
+	}
+	
+}
